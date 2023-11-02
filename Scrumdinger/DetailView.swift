769c66872912fb3bdd33f7012e26cba733bb4c2e
Kunal <kunal17058@gmail.com>
2023-11-02 23:32:53 +0530

    Add Detail View

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -5,4 +5,32 @@
 //  Created by Kunal Yelne on 02/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+        }
+    }
+}
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
