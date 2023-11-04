322b6c48a218c26514a6784c384f61b20611b5b3
Kunal <kunal17058@gmail.com>
2023-11-04 13:22:38 +0530

    Add Detail View and Theme View Files

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Kunal Yelne on 04/11/23.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
