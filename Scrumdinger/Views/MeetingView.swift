9a8e397f8049c0544ec70b117a5b53a9314f5ff6
Kunal <kunal17058@gmail.com>
2024-02-07 00:10:05 +0530

    Add MeetingView changes and custom ProgressViewStyle

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -8,33 +8,32 @@
 import SwiftUI
 
 struct MeetingView: View {
+    
+    @Binding var scrum : DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 10, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("seconds Remaining").font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
-                }
-            }
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: 600, secondsRemaining: 900, theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
                 }
             }
-        }.padding()
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    MeetingView()
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
 }
