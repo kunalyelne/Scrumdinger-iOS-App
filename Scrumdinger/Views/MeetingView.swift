a4e36cde50c7c98b826d7b192ad4e1c5b620557d
Kunal <kunal17058@gmail.com>
2024-02-10 17:16:40 +0530

    Incoporate ScrumTimer in the MeetingView and add MeetingFooterView

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -11,25 +11,28 @@ struct MeetingView: View {
     
     @Binding var scrum : DailyScrum
     
+    @StateObject var scrumTimer = ScrumTimer()
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
-                MeetingHeaderView(secondsElapsed: 600, secondsRemaining: 900, theme: scrum.theme)
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                }
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
         .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
         .navigationBarTitleDisplayMode(.inline)
     }
 }
