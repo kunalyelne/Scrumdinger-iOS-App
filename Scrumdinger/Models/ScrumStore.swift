aa0f784222195fa9ca8ec2d58e0f122aaa12fe98
Kunal <kunal17058@gmail.com>
2024-02-11 23:52:37 +0530

    Add ScrumStore to persist daily scrums

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -5,4 +5,17 @@
 //  Created by Kunal Yelne on 11/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+}
