0d2e049b1de04cd6fd8b6c5febbba82ed42fd4dd
Kunal <kunal17058@gmail.com>
2024-02-19 00:29:50 +0530

    Add MeetingTimerView and Spaker Arc empty path file

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Kunal Yelne on 19/02/24.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingTimerView()
+}
---
+++ Scrumdinger/Views/SpeakerArc.swift
@@ -0,0 +1,18 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by Kunal Yelne on 19/02/24.
+//
+
+import SwiftUI
+
+struct SpeakerArc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeakerArc()
+}
